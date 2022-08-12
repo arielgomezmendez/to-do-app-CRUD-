e9c61df982e203b75c8cdd6510b5801740adf266
Ariel <arielgomezmendez94@gmail.com>
2022-08-11 22:13:14 -0400

    Fix error and get data from local storage

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-/*Select all the selectors from the HTML */
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
@@ -8,56 +7,89 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
-/*validate the input fields*/
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    formValidation(); 
+  e.preventDefault();
+  formValidation();
 });
 
 let formValidation = () => {
-    if (textInput.value === "") {
-      console.log("failure");
-      msg.innerHTML = "Task cannot be blank";
-    } else {
-      console.log("success");
-      msg.innerHTML = "";
-      acceptData(); 
-    }
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
 };
 
-//Collect the data from the input fields, using the function named acceptData and an object named data.
-let data = {};
+let data = [{}];
+
 let acceptData = () => {
-    data["text"] = textInput.value,
-    data["date"] = dateInput.value,
-    data["description"] = textarea.value,
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
 
-    console.log(data);
-    createTasks();
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+  createTasks();
 };
 
-/* Create task, with a function, use template literals to create the HTML elements,
-   and use a map to push the data collected from the user inside the template.  */
 let createTasks = () => {
-    tasks.innerHTML = `
-    <div >
-          <span class="fw-bold">${data.text}</span>
-          <span class="small text-secondary">${data.date}</span>
-          <p>${data.description}</p>
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
   
           <span class="options">
             <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
             <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
           </span>
-    </div>`;
-    resetForm();
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  
+};
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
 };
 
 let resetForm = () => {
-    textInput.value = "";
-    dateInput.value = "";
-    textarea.value = "";
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
 };
 
-
-
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || []
+  console.log(data);
+  createTasks();
+})();
